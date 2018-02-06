98e36648250ccfb52f708ac7eb166a0a0523db6d
Aramis Tanelus <bd543491@ahschool.com>
2018-02-06 15:32:30 -0600

    Changed Solenoid IDs and added gear shifting

--- Limelight Test/src/org/usfirst/frc/team5472/robot/Constants.java
+++ Limelight Test/src/org/usfirst/frc/team5472/robot/Constants.java
@@ -5,33 +5,43 @@ import com.team5472.robot.pathfinder.from_c.fit.FitMethod;
 
 public class Constants {
 	public static final String AUTONOMOUS_CHOOSER_NAME = "Select an Autonomous Program: ";
-	
-	public static enum AutonomousProgram{
+
+	public static enum AutonomousProgram {
 
 		DEFAULT("Drive Straight"), LIMELIGHT("Find the Switch (Limelight Vision)"), JEVOIS("Find the Switch (JeVois Vision)");
-		
+
 		private String title;
+
 		private AutonomousProgram(String title) {
 			this.title = title;
 		}
-		
+
+		@Override
 		public String toString() {
 			return this.title;
 		}
-		
+
 		public String getName() {
 			return this.title;
 		}
-		
+
 	}
-	
-	
+
 	public static final double WHEEL_DIAMETER = 0.10; // meters
-	public static final double ROBOT_WHEELBASE_WIDTH = 0.75; // meters. Distance from center of left wheels to center of right wheels.
+	public static final double ROBOT_WHEELBASE_WIDTH = 0.75; // meters. Distance
+																// from center
+																// of left
+																// wheels to
+																// center of
+																// right wheels.
 	public static final double LEFT_ENCODER_TICKS_PER_METER = 1.0 / 12489.0;
 	public static final double RIGHT_ENCODER_TICKS_PER_METER = 1.0 / 12270.0;
-	public static final double LEFT_ENCODER_TICKS_PER_REV = LEFT_ENCODER_TICKS_PER_METER * 2 * Math.PI * WHEEL_DIAMETER; // ticks / revolution
-	public static final double RIGHT_ENCODER_TICKS_PER_REV = RIGHT_ENCODER_TICKS_PER_METER * 2 * Math.PI * WHEEL_DIAMETER; // ticks / revolution
+	public static final double LEFT_ENCODER_TICKS_PER_REV = LEFT_ENCODER_TICKS_PER_METER * 2 * Math.PI * WHEEL_DIAMETER; // ticks
+																															// /
+																															// revolution
+	public static final double RIGHT_ENCODER_TICKS_PER_REV = RIGHT_ENCODER_TICKS_PER_METER * 2 * Math.PI * WHEEL_DIAMETER; // ticks
+																															// /
+																															// revolution
 	public static final double ROBOT_MAX_VELOCITY = 1.7; // m/s
 	public static final double ENCODER_FOLLOWER_P = 1;
 	public static final double ENCODER_FOLLOWER_I = 0;
@@ -39,21 +49,23 @@ public class Constants {
 	public static final double ENCODER_FOLLOWER_V = 1.0 / ROBOT_MAX_VELOCITY;
 	public static final double ENCODER_FOLLOWER_A = 0;
 	public static final Config TRAJECTORY_CONFIG = new Config(FitMethod.HERMITE_QUINTIC, 1, 0.05, ROBOT_MAX_VELOCITY, 1, 60);
-	
+
 	public static final int INTAKE_LEFT_MOTOR_CAN = 6;
 	public static final int INTAKE_RIGHT_MOTOR_CAN = 7;
-	public static final int INTAKE_SOLENOID_ID_FORW = 0; //It's a double solenoid
-	public static final int INTAKE_SOLENOID_ID_BACK = 1;
+	public static final int INTAKE_SOLENOID_ID_FORW = 1; // It's a double
+															// solenoid
+	public static final int INTAKE_SOLENOID_ID_BACK = 2;
 	public static final double INTAKE_INPUT_SPEED = 0.6;
 	public static final double INTAKE_OUTPUT_SPEED = 1.0;
-	
+
 	public static final int DRIVE_LEFT_TALON_CAN = 1;
 	public static final int DRIVE_LEFT_FOLLOWER_CAN = 2;
 	public static final int DRIVE_RIGHT_TALON_CAN = 4;
 	public static final int DRIVE_RIGHT_FOLLOWER_CAN = 3;
-	
-	public static final int LIFT_TALON_CAN = 5; //TODO
-	
+	public static final int DRIVE_SHIFT_SOLENOID = 0;
+
+	public static final int LIFT_TALON_CAN = 5; // TODO
+
 	public static final double LIMELIGHT_TARGET_AREA_THRESHOLD = 90;
 	public static final double LIMELIGHT_APPROACH_BOX_KP = 0.01;
 }
--- Limelight Test/src/org/usfirst/frc/team5472/robot/subsystems/DriveSubsystem.java
+++ Limelight Test/src/org/usfirst/frc/team5472/robot/subsystems/DriveSubsystem.java
@@ -5,145 +5,154 @@ import org.usfirst.frc.team5472.robot.commands.JoystickDriveCommand;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class DriveSubsystem extends Subsystem{
-	
+public class DriveSubsystem extends Subsystem {
+
 	private AHRS navx;
 	private TalonSRX left, right, followerLeft, followerRight;
 	private ControlMode controlMode;
-//	private Solenoid leftSolenoid, rightSolenoid;
-	
+	private Solenoid shiftSolenoid;// , rightSolenoid;
+
 	public DriveSubsystem() {
 		navx = new AHRS(Port.kMXP);
-		
+
 		left = new TalonSRX(Constants.DRIVE_LEFT_TALON_CAN);
 		right = new TalonSRX(Constants.DRIVE_RIGHT_TALON_CAN);
 		followerLeft = new TalonSRX(Constants.DRIVE_LEFT_FOLLOWER_CAN);
 		followerRight = new TalonSRX(Constants.DRIVE_RIGHT_FOLLOWER_CAN);
-//		leftSolenoid = new Solenoid(RobotMap.leftSolenoid);
-//		rightSolenoid = new Solenoid(RobotMap.rightSolenoid);
-		
+		shiftSolenoid = new Solenoid(Constants.DRIVE_SHIFT_SOLENOID);
+
 		left.setInverted(false);
 		followerLeft.setInverted(false);
 		right.setInverted(true);
 		followerRight.setInverted(true);
-		
-		left.setNeutralMode(NeutralMode.Brake);
-//		followerLeft.setNeutralMode(NeutralMode.Brake);
-		right.setNeutralMode(NeutralMode.Brake);
-//		followerRight.setNeutralMode(NeutralMode.Brake);
-		
-		
-		//Results of experiment:
-		//Left: 12489 Ticks per Meter
-		//Right: 12270 Ticks per Meter
+
+		// left.setNeutralMode(NeutralMode.Brake);
+		// followerLeft.setNeutralMode(NeutralMode.Brake);
+		// right.setNeutralMode(NeutralMode.Brake);
+		// followerRight.setNeutralMode(NeutralMode.Brake);
+
+		// Results of experiment:
+		// Left: 12489 Ticks per Meter
+		// Right: 12270 Ticks per Meter
 		left.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 100);
-//		followerLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 100);
+		// followerLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute,
+		// 0, 100);
 		right.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 100);
-//		followerRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 100);
-		
+		// followerRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute,
+		// 0, 100);
+
 		controlMode = ControlMode.PercentOutput;
-		
+
 		left.set(controlMode, 0);
-		followerLeft.set(ControlMode.Follower, left.getDeviceID());
+		followerLeft.set(controlMode, left.getDeviceID());
 		right.set(controlMode, 0);
-		followerRight.set(ControlMode.Follower, right.getDeviceID());
+		followerRight.set(controlMode, right.getDeviceID());
 	}
-	
+
 	public void setControlMode(ControlMode newMode) {
 		controlMode = newMode;
 	}
-	
+
 	public void drive(double left, double right) {
 		this.left.set(controlMode, left);
 		this.right.set(controlMode, right);
+		this.followerLeft.set(controlMode, left);
+		this.followerRight.set(controlMode, right);
 	}
-	
+
 	@Override
 	protected void initDefaultCommand() {
 		setDefaultCommand(new JoystickDriveCommand());
 	}
-	
-//	public void shiftGear()
-//	{
-//		leftSolenoid.set(!leftSolenoid.get());
-//		rightSolenoid.set(!rightSolenoid.get());
-//	}
-	
+
+	public void shiftGear() {
+		shiftSolenoid.set(!shiftSolenoid.get());
+	}
+
+	public void highGear() {
+		shiftSolenoid.set(false);
+	}
+
+	public void lowGear() {
+		shiftSolenoid.set(true);
+	}
+
 	public void resetEncoders() {
 		left.setSelectedSensorPosition(0, 0, 100);
 		right.setSelectedSensorPosition(0, 0, 100);
 	}
-	
+
 	public int getLeftRaw() {
 		return left.getSelectedSensorPosition(0);
 	}
-	
+
 	public double getLeftPosition() {
 		return left.getSelectedSensorPosition(0) * Constants.LEFT_ENCODER_TICKS_PER_METER;
 	}
-	
+
 	public double getLeftVelocity() {
 		return left.getSelectedSensorVelocity(0) * Constants.LEFT_ENCODER_TICKS_PER_METER;
 	}
-	
+
 	public int getRightRaw() {
 		return right.getSelectedSensorPosition(0);
 	}
-	
+
 	public int getRawRight() {
 		return right.getSelectedSensorPosition(0);
 	}
-	
+
 	public double getRightPosition() {
 		return right.getSelectedSensorPosition(0) * Constants.RIGHT_ENCODER_TICKS_PER_METER;
 	}
-	
+
 	public double getRightVelocity() {
 		return right.getSelectedSensorVelocity(0) * Constants.RIGHT_ENCODER_TICKS_PER_METER;
 	}
-	
+
 	public double getHeading() {
 		return navx.getAngle();
 	}
-	
+
 	public void resetHeading() {
 		navx.zeroYaw();
 	}
-	
+
 	public void reportDebugInformation() {
 		/**
-		 * Reports the following information:
-		 * - Voltage......(For each motor controller)
-		 * - Current......(For each motor controller)
-		 * - Temperature..(For each motor controller)
-		 * - Velocity.....(Left side and Right side)
-		 * - Encoder Readings
-		 * - Bearing......(Relative to starting position of robot, not North)
-		 * - Heading......(Relative to vision target, if any)
+		 * Reports the following information: - Voltage......(For each motor
+		 * controller) - Current......(For each motor controller) -
+		 * Temperature..(For each motor controller) - Velocity.....(Left side
+		 * and Right side) - Encoder Readings - Bearing......(Relative to
+		 * starting position of robot, not North) - Heading......(Relative to
+		 * vision target, if any)
 		 */
 
-//		SmartDashboard.putNumber("Left Voltage", left.getBusVoltage());
-//		SmartDashboard.putNumber("Left Current", left.getOutputCurrent());
-//		SmartDashboard.putNumber("Left Temperature", left.getTemperature());
-//		SmartDashboard.putNumber("Left Output", left.getMotorOutputPercent());
+		// SmartDashboard.putNumber("Left Voltage", left.getBusVoltage());
+		// SmartDashboard.putNumber("Left Current", left.getOutputCurrent());
+		// SmartDashboard.putNumber("Left Temperature", left.getTemperature());
+		// SmartDashboard.putNumber("Left Output",
+		// left.getMotorOutputPercent());
 		SmartDashboard.putNumber("Left Encoder", getLeftPosition());
 		SmartDashboard.putNumber("Left Encoder Velocity", getLeftVelocity());
-//		
-//		
-//		SmartDashboard.putNumber("Right Current", right.getOutputCurrent());
-//		SmartDashboard.putNumber("Right Voltage", right.getBusVoltage());
-//		SmartDashboard.putNumber("Right Temperature", right.getTemperature());
-//		SmartDashboard.putNumber("Right Output", right.getMotorOutputPercent());
+		//
+		//
+		// SmartDashboard.putNumber("Right Current", right.getOutputCurrent());
+		// SmartDashboard.putNumber("Right Voltage", right.getBusVoltage());
+		// SmartDashboard.putNumber("Right Temperature",
+		// right.getTemperature());
+		// SmartDashboard.putNumber("Right Output",
+		// right.getMotorOutputPercent());
 		SmartDashboard.putNumber("Right Encoder", getRightPosition());
 		SmartDashboard.putNumber("Right Encoder Velocity", getRightVelocity());
-//		SmartDashboard.putNumber("Bearing", navx.getAngle());
+		// SmartDashboard.putNumber("Bearing", navx.getAngle());
 	}
 }

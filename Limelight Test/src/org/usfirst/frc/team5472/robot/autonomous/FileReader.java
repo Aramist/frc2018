b8f2e958b506f76710d8ab5f444ed67fc12728fe
Manas Mahajan <mahajanm02@gmail.com>
2018-02-12 19:04:12 -0600

    Test Gib FileReader

---
+++ Limelight Test/src/org/usfirst/frc/team5472/robot/autonomous/FileReader.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team5472.robot.autonomous;
+
+public class FileReader {
+	private String content;
+	private File file;
+
+	public FileReader(String startPosition, String swtichOrScale, String gameData) {
+
+	}
+}

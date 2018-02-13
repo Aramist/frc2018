41e341c9a7af556d98ab357d48c8b264a89caa5e
Manas Mahajan <mahajanm02@gmail.com>
2018-02-12 19:48:25 -0600

    Started Working FileReader

--- Limelight Test/src/org/usfirst/frc/team5472/robot/autonomous/FileReader.java
+++ Limelight Test/src/org/usfirst/frc/team5472/robot/autonomous/FileReader.java
@@ -1,10 +1,34 @@
 package org.usfirst.frc.team5472.robot.autonomous;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
 public class FileReader {
+	private String fileName;
 	private String content;
-	private File file;
+	private BufferedReader buf;
 
-	public FileReader(String startPosition, String swtichOrScale, String gameData) {
-
+	public FileReader(String startPosition, String switchOrScale, String gameData) {
+		fileName = startPosition + switchOrScale + gameData + ".txt";
+		File file = Paths.get("/home/lvuser/stage-one/" + fileName).toFile();
+		try {
+			buf = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public void readFile() {
+		try {
+			content = buf.readLine();
+			System.out.println(content);
+		} catch (Exception e) {
+			System.out.println("Could not GIB");
+		}
+	}
+
 }

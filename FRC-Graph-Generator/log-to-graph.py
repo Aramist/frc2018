8523c2dca336c96dc3ce14dabfdc36bebce8dba5
Aramis Tanelus <bd543491@ahschool.com>
2018-02-24 16:44:20 -0500

    Added more flexibility, needs to fix plot saving

--- FRC-Graph-Generator/log-to-graph.py
+++ FRC-Graph-Generator/log-to-graph.py
@@ -1,96 +1,136 @@
-import os
+from math import ceil
 from os import path
 
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+import numpy as np
 
 
-X_AXIS_LABEL = 'Seconds (Since RoboRIO power-up)'
-Y_AXIS_LABELS = {
-    'lp': 'Encoder Pulses',
-    'lc': 'Amperes',
-    'lo': 'Percent',
-    'do': 'Percent',
-    'dc': 'Amperes',
-    'dp': 'Encoder Pulses',
-    'ip': 'Percent',
-    'ic': 'Amperes',
-    'is': 'Boolean (True = 1.0, False = 0.0)',
-    'lm': 'Boolean (True = 1.0, False = 0.0)',
-    've': 'Boolean (True = 1.0, False = 0.0)',
-    'vt': 'Degrees',
-    'va': 'Percent (of Image Area)',
-    'led': 'Color Value (Between 0 and 255)'
-}
+COLOR_ORDER = 'rbgkmcy'
 
-GRAPH_TYPES = {
-    'lp': 'Lift Position',
-    'lc': 'Lift Current',
-    'lo': 'Lift Output Percent',
-    'do': 'Drive Motor Output Percent',
-    'dc': 'Drive Motor Output Current',
-    'dp': 'Drive Motor Encoder Position',
-    'ip': 'Intake Output Percent',
-    'ic': 'Intake Output Current',
-    'is': 'Intake Solenoid States',
-    'lm': 'Limelight Connected',
-    've': 'Vision Target Exists',
-    'vt': 'Vision Target Angle',
-    'va': 'Vision Target Area',
-    'led': 'LED Color'
-}
 
-INVERTED_GRAPH_TYPES = {b: a for a,b in GRAPH_TYPES.items()}
+class DataCategory(object):
+	def __init__(self, formal, short, unit, labels=None):
+		self.formal_name = formal
+		self.short_name = short;
+		self.unit = unit
+		self.has_subcategories = False
+		self.values = list()
+		self.labels = labels
+		if labels is None:
+			self.labels = [self.formal_name]
+	
+	def __str__(self):
+		return 'DataCategory {}'.format(self.formal_name)
+	
+	def add_value(self, value):
+		if len(self.values) == 0 and type(value) is list:
+			for _ in range(len(value)): self.values.append(list())
+		if not type(value) is list:
+			self.values.append(value)
+			return
+		self.has_subcategories = True
+		for num, item in enumerate(value):
+			self.values[num].append(value)
 
-file_name = input('Path to data log: ').strip()
-file_path = path.abspath(file_name)
+	def label(self):
+		return self.formal_name
 
-print('Which graph should be generated? Use the shorthand labels given in parentheses as your input.')
-for key,value in GRAPH_TYPES.items():
-    print('{} ({})'.format(key, value))
+	def short(self):
+		return self.short_name
 
-graph_type = ''
-while not graph_type in GRAPH_TYPES:
-    graph_type = input('Graph to generate: ').replace('(', '').replace(')', '').strip().lower()
+	def unit(self):
+		return self.unit
 
-modifier = ''
-if graph_type in ('do', 'dc'):
-    print('Front Left (fl)')
-    print('Front Right (fr)')
-    print('Back Left (bl)')
-    print('Back Right (br)')
-    while not modifier in ('fr', 'fl', 'br', 'bl'):
-        modifier = input('Choose a motor: ')
-if graph_type in ('dp', 'ip', 'ic'):
-    print('Left (l)')
-    print('Right (r)')
-    while not modifier in ('l', 'r'):
-        modifier = input('Choose a side: ')
+	def plot(self):
+		if self.has_subcategories:
+			for num, value_list in enumerate(self.values):
+				plt.plot(TIMESTAMPS, value_list, COLOR_ORDER[num] + '-', label=self.labels[num])
+		else:
+			plt.plot(TIMESTAMPS, self.values, COLOR_ORDER[0] + '-', label=self.labels[0])
 
-timestamps = list()
-y_axis = list()
-with open(file_path, 'r') as ctx:
-    for line in ctx:
-        if line.startswith('Timestamp'):
-            timestamps.append(float(line.split(': ')[1]))
-        else:
-            _split_line = line.split('[')
-            key = _split_line[0].replace('\t', '').replace(': ', '')
-            if not(key in INVERTED_GRAPH_TYPES and INVERTED_GRAPH_TYPES[key] == graph_type):
-                continue
-            value_str = _split_line[1].replace(']', '')
-            values = [float(b.strip()) for b in value_str.split(',')]
-            if modifier == '':
-                y_axis.append(values[0])
-            elif modifier in ('l', 'r'):
-                y_axis.append(values[('l','r').index(modifier)])
-            elif modifier in ('fl', 'fr', 'bl', 'br'):
-                y_axis.append(values[('fl', 'fr', 'bl', 'br').index(modifier)])
 
-plt.plot(timestamps, y_axis, 'r-')
-plt.title(GRAPH_TYPES[graph_type])
-plt.xlabel(X_AXIS_LABEL)
-plt.ylabel(Y_AXIS_LABELS[graph_type])
-save = input('Would you like to save the graph? ')
-if save == 'yes' or save == 'y':
-    plt.savefig('{}.png'.format(GRAPH_TYPES[graph_type]))
-plt.show()
+# Input your data categories here
+TIMESTAMPS = list() # The x-axis
+CATEGORIES = [
+	DataCategory('Lift Position', 'lp', 'Encoder Pulses'),
+	DataCategory('Lift Current', 'lc', 'Amperes', ['One', 'Two']),
+	DataCategory('Lift Output Percent', 'lo', 'Percent'),
+	DataCategory('Drive Motor Output Percent', 'do', 'Percent', ['Front Left', 'Front Right', 'Back Left', 'Back Right']),
+	DataCategory('Drive Motor Output Current', 'dc', 'Amperes', ['Front Left', 'Front Right', 'Back Left', 'Back Right']),
+	DataCategory('Drive Motor Encoder Position', 'dp', 'Encoder Pulses', ['Front Left', 'Front Right', 'Back Left', 'Back Right']),
+	DataCategory('Drive Motor Encoder Velocity', 'dv', 'Meters per 100ms', ['Front Left', 'Front Right', 'Back Left', 'Back Right']),
+	DataCategory('Intake Output Percent', 'ip', 'Percent', ['Left', 'Right']),
+	DataCategory('Intake Output Current', 'ic', 'Amperes', ['Left', 'Right']),
+	DataCategory('Intake Solenoid State', 'is', 'Forward = 1.0, Reverse = -1.0)'),
+	DataCategory('Limelight Connected', 'lm', 'Boolean (True = 1.0, False = 0.0)'),
+	DataCategory('Vision Target Exists', 've', 'Boolean (True = 1.0, False = 0.0)'),
+	DataCategory('Vision Target Angle', 'vt', 'Degrees'),
+	DataCategory('Vision Target Area', 'va', 'Percent (of Image Area)'),
+	DataCategory('LED Color', 'led', 'Color Value (Between 0 and 255)', ['Red', 'Green', 'Blue']),
+	DataCategory('CAN Bus Utilization', 'can', 'Percent'),
+	DataCategory('Brown Out', 'bo', 'Boolean (True = 1.0, False = 0.0)'),
+	DataCategory('Battery Voltage', 'bv', 'Volts'),
+	DataCategory('Pressure', 'psi', 'psig')
+]
+# End of data category section
+
+
+LOOKUP_DICT = {b.label(): b for b in CATEGORIES} # A dictionary to get DataCategory from its formal name
+
+
+def populate_categories(file_path):
+	file_path = path.abspath(file_path)
+	with open(file_path, 'r') as ctx:
+		for line in ctx:
+			if line.startswith('Timestamp'):
+				TIMESTAMPS.append(float(line.split(': ')[1]))
+			else:
+				split_middle = line.split(': ')
+				if len(split_middle) <= 1: continue
+				key = split_middle[0].replace('\t', '').strip()
+				value_str = split_middle[1].replace('[', '').replace(']', '').strip()
+				values = [float(b.strip()) for b in value_str.split(',')]
+				if key in LOOKUP_DICT:
+					LOOKUP_DICT[key].add_value(values[0] if len(values) == 1 else values)
+
+
+def data_category_from_shorthand(short):
+	for category in CATEGORIES:
+		if category.short() == short:
+			return category
+	return None
+
+
+def run_with_user_input():
+	file_name = input('Path to the data log: ')
+	print('Populating data containers...')
+	populate_categories(file_name)
+	global TIMESTAMPS
+	TIMESTAMPS = np.array(TIMESTAMPS) - TIMESTAMPS[0]
+	print('Done populating containers')
+	print('Enter the shorthand titles, comma separated, of the categories for which you would like to generate graphs.')
+	[print('{} ({})'.format(a.label(), a.short())) for a in CATEGORIES]
+	selected_categories = input('Select one (or more) categories: ').strip().split(',')
+	selected_categories = [a.strip().lower() for a in selected_categories]
+	data_categories = list()
+	for selected in selected_categories:
+		category = data_category_from_shorthand(selected)
+		if category is None: continue
+		data_categories.append(category)
+	print('Generating plots...')
+	do_plots(data_categories)
+
+
+def do_plots(categories_to_plot):
+	for category in categories_to_plot:
+		category.plot()
+		plt.title(category.label())
+		plt.show()
+		print('Would you like to save the plot of {}? '.format(category.label()))
+		save = input('Save? (y/n)')
+		if save.lower().strip() == 'y':
+			plt.savefig(category.label())
+
+if __name__ == '__main__':
+	run_with_user_input()
